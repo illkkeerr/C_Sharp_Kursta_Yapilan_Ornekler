9ff8a73d143816e4c7fca1f30e4040b3dc2d5131
illkkeerr <ilkersenel5797@gmail.com>
2023-05-10 02:38:45 +0300

    C-Sharp_Kursta_Yapilan_Ornekler
    Aldığım C# kurs eğitiminde yapmış olduğumuz örnekler ve uygulamalar.
    Yapılan örnekleri ve uygulamaları düzenledikçe proje güncellenecek.

---
+++ Konsol_Uygulamaları/Konsol_Uygulamaları/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsol_Uygulamaları
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Yazılım geliştirme kursundan yaptığımız örnekleri düzeneleyerek github sayfamdan payşacağım 
+           //her düzenleme yaptıktan sonra proje dosyları güncellenecek
+
+
+        }
+    }
+}
